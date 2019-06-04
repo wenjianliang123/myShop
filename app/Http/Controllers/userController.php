0e8743906dcbeb0308a5696dc97c3a31c005a02f
wenjianliang <1321805139@qq.com>
2019-06-04 15:55:22 +0800

    orgin

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    public function index()
+    {
+        for ($i=1;$i<10;$i++){
+            echo $i;
+            echo "<br />";
+        }
+        return view('welcome');
+    }
+}
